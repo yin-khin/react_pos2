0c981fc585d5cc1406f9919327e0e4c8660f9692
prengkhmer <prengkhmer1999@gmail.com>
2026-03-03 20:22:14 +0700

    first commit

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -1,27 +1,57 @@
+// import axios from 'axios';
+
+
+// var baseURL = "http://localhost:3000/";
+// const request = (path="",method="GET" , data={}) =>{
+//     const isFormData = data instanceof FormData;
+
+//     return axios({
+//         method:method,
+//         url:baseURL+path,
+//         data :data,
+//         headers : isFormData
+//             ? {}
+//             : {
+//                 'Content-Type' : 'application/json'
+//             }
+//     })
+//     .then((response) =>{
+//         return response.data;
+//     })
+//     .catch((error) =>{
+//         console.log(error);
+//         throw error;
+//     })
+// }
+
+// export default request;
+
 import axios from 'axios';
+const baseURL = import.meta.env.VITE_BASE_URL || "http://localhost:3000/";
 
-
-var baseURL = "http://localhost:3000/";
-const request = (path="",method="GET" , data={}) =>{
-    const isFormData = data instanceof FormData;
-
-    return axios({
-        method:method,
-        url:baseURL+path,
-        data :data,
-        headers : isFormData
-            ? {}
-            : {
-                'Content-Type' : 'application/json'
+const request = async (path = "", method = "GET", data = {}) => {
+    const safeMethod = (typeof method === 'string' ? method : 'GET').toUpperCase();
+    
+    try {
+        const config = {
+            method: safeMethod,
+            url: baseURL + path,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token') || ''}`
             }
-    })
-    .then((response) =>{
+        };
+        if (safeMethod === 'GET') {
+            config.params = data;
+        } else {
+            config.data = data;
+        }
+        const response = await axios(config);
         return response.data;
-    })
-    .catch((error) =>{
-        console.log(error);
-        throw error;
-    })
-}
+    } catch (error) {
+        console.error("API Request Error:", error.response?.data || error.message);
+            throw error;
+    }
+};
 
 export default request;
